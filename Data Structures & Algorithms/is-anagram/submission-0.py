51cc16046db72b04d501877792984a2a850cd00c
Oleg Golovin <ovgolovin@gmail.com>
2026-03-29 16:50:17 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        def get_counts(a):
+            counts = {}
+            for letter in a:
+                counts[letter] = counts.get(letter, 0) + 1
+            return counts
+        
+        s_counts = get_counts(s)
+        t_counts = get_counts(t)
+
+        if len(s_counts.keys()) != len(t_counts.keys()):
+            return False
+
+        for letter, s_count in s_counts.items():
+            t_count = t_counts.get(letter, 0)
+
+            if t_count != s_count:
+                return False
+        
+        return True
