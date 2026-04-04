f0ca6a94ef86b614342c11d7e8cb92bed3a9c571
Oleg Golovin <ovgolovin@gmail.com>
2026-04-03 19:49:19 -0700

    Add: binary-search - submission-8

---
+++ Data Structures & Algorithms/binary-search/submission-8.py
@@ -0,0 +1,43 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        return self.search_lower_bound(nums, target)
+
+
+    def search_iterative(self, nums: List[int], target: int) -> int:
+        i = 0
+        j = len(nums) - 1
+        while i <= j:
+            mid = i + (j - i) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                i = mid + 1
+            else:
+                j = mid - 1
+        return -1
+
+
+    def search_lower_bound(self, nums: List[int], target: int) -> int:
+        # first index where a value is greater than or equal to the target
+        l, r = 0, len(nums)
+
+        while l < r:
+            m = l + ((r - l) // 2)
+            if nums[m] >= target:
+                r = m
+            else:
+                l = m + 1
+        return l if (l < len(nums) and nums[l] == target) else -1
+
+
+    def search_upper_bound(self, nums: List[int], target: int) -> int:
+        # first index where a value greater than the target appears
+        l, r = 0, len(nums)
+
+        while l < r:
+            m = l + ((r - l) // 2)
+            if nums[m] > target:
+                r = m
+            else:
+                l = m + 1
+        return l - 1 if (l and nums[l - 1] == target) else -1
