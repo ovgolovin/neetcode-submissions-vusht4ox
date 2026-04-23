121e42e19723f4dd44b0f541a45c6a775a9ba42c
Oleg Golovin <ovgolovin@gmail.com>
2026-04-22 18:53:26 -0700

    Add: binary-search - submission-9

---
+++ Data Structures & Algorithms/binary-search/submission-9.py
@@ -0,0 +1,120 @@
+import bisect
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        index = self.bisect_right(nums, target)
+        return index - 1 if index > 0 and nums[index - 1] == target else -1
+
+
+    def bisect_right(self, nums, target):
+        l = 0
+        r = len(nums)
+
+        while l < r:
+            mid = l + (r - l) // 2
+            if nums[mid] <= target:
+                l = mid + 1
+            else:
+                r = mid
+        return l
+
+
+
+    def bisect_left(self, nums, target):
+        l = 0
+        r = len(nums)
+
+        while l < r:
+            mid = l + (r - l) // 2
+            if nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid
+        
+        return l
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def search_iterative(self, nums: List[int], target: int) -> int:
+        i = 0
+        j = len(nums) - 1
+        while i <= j:
+            mid = i + (j - i) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                i = mid + 1
+            else:
+                j = mid - 1
+        return -1
+
+
+    def search_lower_bound(self, nums: List[int], target: int) -> int:
+        # first index where a value is greater than or equal to the target
+        l, r = 0, len(nums)
+
+        while l < r:
+            m = l + ((r - l) // 2)
+            if nums[m] >= target:
+                r = m
+            else:
+                l = m + 1
+        return l if (l < len(nums) and nums[l] == target) else -1
+
+
+    def search_upper_bound(self, nums: List[int], target: int) -> int:
+        # first index where a value greater than the target appears
+        l, r = 0, len(nums)
+
+        while l < r:
+            m = l + ((r - l) // 2)
+            if nums[m] > target:
+                r = m
+            else:
+                l = m + 1
+        return l - 1 if (l and nums[l - 1] == target) else -1
