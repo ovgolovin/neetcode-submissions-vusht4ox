dc075d2611955f630b6c6c753a891afe1d6bf557
Oleg Golovin <ovgolovin@gmail.com>
2026-05-08 18:31:07 -0700

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        # Stack holds (index, current_subset)
+        stack = [(0, [])]
+        
+        while stack:
+            i, subset = stack.pop()
+            
+            if i >= len(nums):
+                res.append(subset)
+                continue
+            
+            # Branch 1: skip nums[i]
+            stack.append((i + 1, subset[:]))
+            
+            # Branch 2: include nums[i]
+            stack.append((i + 1, subset + [nums[i]]))
+        
+        return res
