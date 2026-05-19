2d4de3b6e8cf9a462c9224fcdbd24ac82b6863c9
Oleg Golovin <ovgolovin@gmail.com>
2026-05-18 17:30:47 -0700

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+
+        moves = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        grid = [row[:] for row in grid]
+
+        def explore(i, j):
+            stack = [(i, j)]
+            res = 0
+
+            while stack:
+                i, j = stack.pop()
+                if i < 0 or j < 0 or i >= n or j >= m or grid[i][j] == 0:
+                    continue
+                grid[i][j] = 0
+                res += 1
+                for mx, my in moves:
+                    stack.append((i + mx, j + my))
+            return res
+
+        return max((explore(i, j) for i in range(n) for j in range(m)), default=0)
+
+        
