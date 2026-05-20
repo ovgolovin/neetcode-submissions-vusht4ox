8139fda8d723204b60c2cc8fe9039cf950fd281c
Oleg Golovin <ovgolovin@gmail.com>
2026-05-20 13:44:21 -0700

    Add: pow-x-n - submission-8

---
+++ Data Structures & Algorithms/pow-x-n/submission-8.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+
+        double res = 1;
+        long power = abs((long)n);
+
+        while (power) {
+            if (power & 1) {
+                res *= x;
+            }
+            x *= x;
+            power >>= 1;
+        }
+
+        return n >= 0 ? res : 1 / res;
+    }
+};
