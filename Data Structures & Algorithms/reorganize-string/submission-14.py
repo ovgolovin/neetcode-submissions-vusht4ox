81e9d7392937c819c4ebc143d8ccddded5fa7f33
Oleg Golovin <ovgolovin@gmail.com>
2026-06-15 23:00:53 -0700

    Add: reorganize-string - submission-14

---
+++ Data Structures & Algorithms/reorganize-string/submission-14.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        counts = Counter(s)
+        most_frequent = max(counts.items(), key=lambda x: x[1])
+        if most_frequent[1] > (len(s) + 1) // 2:
+            return ""
+
+        def chars_in_order():
+            for _ in range(most_frequent[1]):
+                yield most_frequent[0]
+
+            for char, count in counts.items():
+                if char == most_frequent[0]:
+                    continue
+                for _ in range(count):
+                    yield char
+
+        res = [None] * len(s)
+
+        indices = (i for offset in (0, 1) for i in range(0 + offset, len(s), 2))
+        for i, char in zip(indices, chars_in_order()):
+            res[i] = char
+
+        return ''.join(res)
+
+        
