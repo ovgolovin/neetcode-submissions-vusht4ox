a2494528d6c33c676b0e105f744f4f5ece92dc9c
Oleg Golovin <ovgolovin@gmail.com>
2026-06-28 20:05:52 -0700

    Add: delete-node-in-a-bst - submission-5

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-5.py
@@ -0,0 +1,57 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+
+        par = None
+        curr = root
+        while True:
+            if not curr:
+                return root
+            elif key == curr.val:
+                break
+            elif key < curr.val:
+                par = curr
+                curr = curr.left
+            else:
+                par = curr
+                curr = curr.right
+
+        if not curr.left or not curr.right:
+            child = curr.left or curr.right
+            if par is None:
+                return child
+            elif par.left is curr:
+                par.left = child
+            else:
+                par.right = child
+            return root
+
+        
+        tmp = curr.right
+        tmp_par = None
+        while tmp.left:
+            tmp_par = tmp
+            tmp = tmp.left
+        
+        if tmp_par:
+            tmp_par.left = tmp.right
+            tmp.right = curr.right
+
+        tmp.left = curr.left
+
+        if par is None:
+            return tmp
+        elif par.left is curr:
+            par.left = tmp
+        else:
+            par.right = tmp            
+
+        return root
+        
