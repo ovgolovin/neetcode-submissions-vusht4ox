2fe11fb924b6044b4468d60e172a53aa07adfc1e
Oleg Golovin <ovgolovin@gmail.com>
2026-07-15 19:33:12 -0700

    Add: matchsticks-to-square - submission-2

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-2.py
@@ -0,0 +1,46 @@
+from typing import List
+
+
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        total = sum(matchsticks)
+
+        if total % 4 != 0:
+            return False
+
+        target = total // 4
+
+        if max(matchsticks) > target:
+            return False
+
+        sticks = tuple(sorted(matchsticks, reverse=True))
+
+        def dfs(
+            deferred: tuple[int, ...],
+            available: tuple[int, ...],
+            current_sum: int,
+        ) -> bool:
+            if not available:
+                return False
+
+            stick = available[0]
+            rest = available[1:]
+
+            if current_sum + stick <= target:
+                next_sum = (current_sum + stick) % target
+
+                if next_sum == 0:
+                    next_available = deferred + rest
+
+                    if not next_available:
+                        return True
+
+                    if dfs((), next_available, 0):
+                        return True
+
+                elif dfs(deferred, rest, next_sum):
+                    return True
+
+            return dfs(deferred + (stick,), rest, current_sum)
+
+        return dfs((), sticks, 0)
