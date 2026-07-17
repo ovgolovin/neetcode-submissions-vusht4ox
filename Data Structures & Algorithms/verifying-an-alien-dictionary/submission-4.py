614e7e09730954d45626cc8e1c8fa2c5c04162fa
Oleg Golovin <ovgolovin@gmail.com>
2026-07-16 21:59:56 -0700

    Add: verifying-an-alien-dictionary - submission-4

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-4.py
@@ -0,0 +1,18 @@
+from itertools import pairwise
+from string import ascii_lowercase
+
+
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        translation = str.maketrans(order, ascii_lowercase[:len(order)])
+        translated = (word.translate(translation) for word in words)
+        for word1, word2 in pairwise(translated):
+            if word1 > word2:
+                return False
+        return True
+
+
+
+
+
+        
