792142a630207c8286af1b8563797cf3a92b1139
Oleg Golovin <ovgolovin@gmail.com>
2026-07-22 09:48:42 -0700

    Add: delete-leaves-with-a-given-value - submission-1

---
+++ Data Structures & Algorithms/delete-leaves-with-a-given-value/submission-1.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+
+        def dfs(node):
+            if node is None:
+                return None
+
+            node.left = dfs(node.left)
+            node.right = dfs(node.right)
+
+            if node.left or node.right:
+                return node
+            elif node.val == target:
+                return None
+            else:
+                return node
+
+
+        return dfs(root)
+                
+
+        
