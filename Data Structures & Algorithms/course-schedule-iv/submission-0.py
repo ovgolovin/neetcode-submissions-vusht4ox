f43f7ae75c0dbb989453b023876c8e97207beb30
Oleg Golovin <ovgolovin@gmail.com>
2026-08-09 17:30:54 -0700

    Add: course-schedule-iv - submission-0

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        indegree = [0] * numCourses
+        adj = [set() for _ in range(numCourses)]
+
+        for src, dst in prerequisites:
+            indegree[dst] += 1
+            adj[src].add(dst)
+
+        ancestors = [set() for _ in range(numCourses)]
+
+        queue = deque((i for i in range(numCourses) if indegree[i] == 0))
+
+        while queue:
+            node = queue.popleft()
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    queue.append(nei)
+                
+                ancestors[nei].add(node)
+                ancestors[nei].update(ancestors[node])
+
+        print(ancestors)
+
+        return [src in ancestors[dst] for src, dst in queries]
