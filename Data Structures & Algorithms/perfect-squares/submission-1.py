b4be78b2e9ba6a9c72a3722c669e56309341d577
Oleg Golovin <ovgolovin@gmail.com>
2026-08-16 16:42:44 -0700

    Add: perfect-squares - submission-1

---
+++ Data Structures & Algorithms/perfect-squares/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [n] * (n + 1)
+        dp[0] = 0
+
+        for target in range(1, n + 1):
+            for cand in range(1, n + 1):
+                squared = cand * cand
+                if squared > target:
+                    break
+
+                dp[target] = min(dp[target], 1 + dp[target - squared])
+
+        return dp[n]    
+            
