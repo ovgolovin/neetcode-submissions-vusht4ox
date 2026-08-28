aa0aaebf0df9c2787d81dc9c33e20e0a3860f963
Oleg Golovin <ovgolovin@gmail.com>
2026-08-28 16:04:04 -0700

    Add: lowest-common-ancestor-of-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        p_path = q_path = None
+        path = []
+
+        def dfs(node):
+            nonlocal p_path, q_path
+            if node is None or (p_path and q_path):
+                return
+
+            path.append(node)
+
+            if node is p:
+                p_path = path.copy()
+            elif node is q:
+                q_path = path.copy()
+
+            dfs(node.left)
+            dfs(node.right)
+
+            path.pop()
+
+        dfs(root)
+
+        lca = None
+        for a, b in zip(p_path, q_path):
+            if a is not b:
+                break
+            lca = a
+        return lca
